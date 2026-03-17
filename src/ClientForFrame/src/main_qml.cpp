1c936e6792852fa2dc1dec4a8397e1cd5082ca8a
ChrisYan03 <545047245@qq.com>
2026-03-17 21:59:12 +0800

    优化日志打印

--- src/ClientForFrame/src/main_qml.cpp
+++ src/ClientForFrame/src/main_qml.cpp
@@ -13,7 +13,35 @@
 #include <QTimer>
 #include <QDir>
 #include <QCoreApplication>
+#include <QtGlobal>
 #include "LogUtil.h"
+
+// 全局 Qt 消息处理，将 qDebug/qInfo/qWarning/qCritical/qFatal 输出统一转发到 LogUtil(spdlog)
+static void QtLogMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    const QByteArray localMsg = msg.toLocal8Bit();
+    const char* file = context.file ? context.file : "";
+    int line = context.line;
+    const char* function = context.function ? context.function : "";
+
+    switch (type) {
+    case QtDebugMsg:
+        LOG_DEBUG("{} ({}:{}:{})", localMsg.constData(), file, line, function);
+        break;
+    case QtInfoMsg:
+        LOG_INFO("{} ({}:{}:{})", localMsg.constData(), file, line, function);
+        break;
+    case QtWarningMsg:
+        LOG_WARN("{} ({}:{}:{})", localMsg.constData(), file, line, function);
+        break;
+    case QtCriticalMsg:
+        LOG_ERROR("{} ({}:{}:{})", localMsg.constData(), file, line, function);
+        break;
+    case QtFatalMsg:
+        LOG_CRITICAL("{} ({}:{}:{})", localMsg.constData(), file, line, function);
+        abort();
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -32,6 +60,7 @@ int main(int argc, char *argv[])
     QDir().mkpath("logs");
 
     LogUtil::initLogger("ClientApp");
+    qInstallMessageHandler(QtLogMessageHandler);
     LOG_INFO("-------------------------------Application starting (QML)...");
 
     // 获取应用基础路径
