64467f85b273f47a15554f2a17e27be25f19b88d
ChrisYan03 <545047245@qq.com>
2026-03-25 21:53:27 +0800

    新增imageview组件

---
+++ src/Component/ImageView_Component/deps/PicPlayer/include/PicPlayerGlobal.h
@@ -0,0 +1,22 @@
+﻿#ifndef PICPLAYER_GLOBAL_H
+#define PICPLAYER_GLOBAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    #ifdef _WIN32
+        #ifdef PICPLAYER_EXPORTS
+            #define PICPLAYER_API __declspec(dllexport)
+        #else
+            #define PICPLAYER_API __declspec(dllimport)
+        #endif
+        #define PICPLAYER_CALL __stdcall
+    #else
+        #define PICPLAYER_API __attribute__((visibility("default")))
+        #define PICPLAYER_CALL
+    #endif
+#ifdef __cplusplus
+}
+#endif
+#endif // PICPLAYER_GLOBAL_H
+
